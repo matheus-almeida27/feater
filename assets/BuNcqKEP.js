e0f13349334006be0d123f2d068884a2f5800392
matheus-almeida27 <matheus.fdealmeida@hotmail.com>
2025-04-03 00:37:53 -0300

    Updates

--- assets/BuNcqKEP.js
+++
@@ -1,1 +0,0 @@
-import{X as t,o as e,j as s,aF as n}from"./DrTNfSRB.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
