fbd85544fbca124f3fa7c5e5387ed38fdafe9403
matheus-almeida27 <matheus.fdealmeida@hotmail.com>
2025-04-11 02:41:37 -0300

    reafc: mensagens e perfil do matchedUser

--- pages/messages/[chatId].vue
+++ pages/messages/[chatId].vue
@@ -10,53 +10,72 @@
 			@click="goBack">
 			<v-icon>mdi-arrow-left</v-icon>
 		</v-btn>
-		<v-avatar
-			size="40"
-			class="ml-3 mr-2">
-			<v-img
-				contain
-				:src="matchUser?.profileImage || ''"
-				:alt="matchUser?.name" />
-		</v-avatar>
-		<v-toolbar-title class="font-weight-light">{{ matchUser?.name || "Chat" }}</v-toolbar-title>
+		<div
+			@click="openMatchedProfile"
+			:ripple="true"
+			class="d-flex w-100 align-center cursor-pointer">
+			<v-avatar
+				size="40"
+				class="ml-0 mr-3">
+				<v-img
+					contain
+					:src="matchUser?.profileImage || ''"
+					:alt="matchUser?.name" />
+			</v-avatar>
+			<v-toolbar-title class="font-weight-light">{{
+				matchUser?.name || "Chat"
+			}}</v-toolbar-title>
+		</div>
 	</v-app-bar>
+	<!-- um dialog para exibir as informações do perfil do usuário que deu match -->
+	<v-dialog
+		v-model="profileDialog"
+		max-width="600px"
+		transition="dialog-transition">
+		<profile-card />
+	</v-dialog>
+
 	<v-container
 		fluid
-		class="overflow-y-auto h-100 d-flex flex-column align-center pa-0">
+		class="overflow-y-auto h-100 d-flex flex-column align-center pa-0 message-container"
+		id="message_container">
 		<v-col
 			cols="12"
 			lg="6"
 			md="8"
-			sm="10"
-			class="d-flex justify-center">
+			class="d-flex justify-center pb-0">
 			<v-list
 				class="pa-0 bg-transparent w-100"
 				two-line>
-				<v-list-item
+				<div
 					v-for="message in chat?.messages || []"
 					:key="message.id"
+					@click="showMessageTime = !showMessageTime"
 					:class="{
 						sent: message.sender === authStore.user?.id,
 						received: message.sender !== authStore.user?.id,
 					}"
 					class="message-item">
-					<v-avatar
-						v-if="message.sender !== authStore.user?.id"
-						size="30"
-						class="mr-1">
-						<v-img
-							contain
-							:src="matchUser?.profileImage || ''"
-							:alt="matchUser?.name" />
-					</v-avatar>
-					<v-list-item-title class="message-text">
+					<div class="message-text">
 						{{ message.text }}
-					</v-list-item-title>
-					<v-list-item-subtitle class="message-time">
-						{{ formatTimestamp(message.timestamp) }}
-					</v-list-item-subtitle>
-				</v-list-item>
+						<v-expand-transition>
+							<div
+								class="message-time"
+								v-if="showMessageTime">
+								{{ formatTimestamp(message.timestamp) }}
+							</div>
+						</v-expand-transition>
+					</div>
+				</div>
 			</v-list>
+		</v-col>
+	</v-container>
+
+	<!-- Campo de texto fixado -->
+	<v-bottom-navigation
+		app
+		height="auto">
+		<div class="input-wrapper">
 			<v-text-field
 				v-model="newMessage"
 				placeholder="Digite sua mensagem..."
@@ -69,120 +88,138 @@
 				color="purple-darken-3"
 				@keyup.enter="sendMessage"
 				append-inner-icon="mdi-send"
-				@click:append-inner="sendMessage">
-			</v-text-field>
-		</v-col>
-	</v-container>
+				@click:append-inner="sendMessage"></v-text-field>
+		</div>
+	</v-bottom-navigation>
 </template>
 
 <script setup lang="ts">
 	definePageMeta({
 		layout: "chat",
 	});
-	import { ref, computed } from "vue";
+	import { ref, computed, onMounted, nextTick } from "vue";
 	import { useRoute, useRouter } from "vue-router";
 	import type { Chat, User, Message } from "~/types/types.global";
 
-	// Acessa as stores (ajuste os paths conforme seu projeto)
-	const authStore = useAuthStore(); // authStore.user é o usuário logado
-	const staticStore = useStaticStore(); // staticStore.users contém os dados dos usuários
-	const chatsStore = useChatsStore(); // chatsStore.chats contém os chats
+	// Acessa as stores
+	const authStore = useAuthStore();
+	const staticStore = useStaticStore();
+	const chatsStore = useChatsStore();
+	const showMessageTime = ref(false);
+	const profileDialog = ref(false);
+	const matchedUserId = ref();
 
 	const route = useRoute();
 	const router = useRouter();
 	const chatId = Number(route.params.chatId);
 
-	// Procura o chat com base no chatId (lembre-se que o tipo Chat possui id como number)
 	const chat = chatsStore.chats.find((c: Chat) => c.id === chatId);
 
 	if (!chat) {
-		// Se o chat não for encontrado, você pode redirecionar ou mostrar uma mensagem de erro
 		router.push("/matches");
 	}
 
-	// Computa o matchUser: o outro usuário que participa do chat
 	const matchUser = computed(() => {
 		const currentUserId = authStore.user?.id;
 		const otherUserId = chat?.participants.find((id: number) => id !== currentUserId);
 		return staticStore.users.find((user: User) => user.id === otherUserId);
 	});
 
-	// Variável para nova mensagem
 	const newMessage = ref("");
+	let messageContainer: any;
 
-	// Função para formatar o timestamp
 	function formatTimestamp(timestamp: string): string {
+		//formatar o timestamp para exibir no formato DD MES AA HH:mm
 		const date = new Date(timestamp);
-		return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+		const options: Intl.DateTimeFormatOptions = {
+			day: "2-digit",
+			month: "long",
+			year: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+		};
+		return date.toLocaleString("pt-BR", options);
 	}
 
-	// Função para enviar mensagem
 	function sendMessage() {
 		if (!newMessage.value.trim() || !chat) return;
 
 		const message: Message = {
-			id: Date.now(), // Pode usar uma estratégia de geração de ID melhor em produção
+			id: Date.now(),
 			chatId: chat.id,
 			sender: authStore?.user?.id || 0,
 			text: newMessage.value.trim(),
 			timestamp: new Date().toISOString(),
 		};
 
-		// Adiciona a mensagem ao array do chat
 		chat.messages.push(message);
-
-		// Limpa o campo de entrada
 		newMessage.value = "";
 
-		// Opcional: implementar scroll automático para as mensagens recentes
+		// Scroll para a última mensagem
+		scrollToBottom();
 	}
 
-	// Função para voltar à tela anterior
+	function scrollToBottom() {
+		nextTick(() => {
+			if (messageContainer) {
+				messageContainer.scrollTo(0, messageContainer.scrollHeight);
+			}
+		});
+	}
+
+	function openMatchedProfile() {
+		if (matchUser.value) {
+			// Passa o usuário correspondente para o componente de perfil
+			matchedUserId.value = matchUser.value.id;
+			profileDialog.value = true;
+		}
+	}
+
 	function goBack() {
 		router.back();
 	}
+
+	// Scroll automático ao carregar mensagens
+	onMounted(() => {
+		nextTick(() => {
+			messageContainer = document.getElementById("message_container");
+			// scrollToBottom();
+		});
+	});
 </script>
 
 <style scoped lang="scss">
-	.fill-height {
-		height: calc(100vh - 80px) !important;
-		margin-top: 80px !important;
-	}
-
 	.message-container {
 		overflow-y: auto;
 		max-height: calc(100vh - 128px); /* Ajusta conforme altura do app-bar e footer */
+		flex: 1;
 	}
 
-	.message-item:deep(.v-list-item__content) {
-		/* Estilo para o item da lista de mensagens */
-		width: 100%;
-	}
 	.message-item {
-		/* Espaçamento e alinhamento para diferenciar mensagens enviadas e recebidas */
-		display: flex;
+		display: flex !important;
+		flex-direction: column;
 		margin-bottom: 12px;
 	}
 
 	.sent {
-		justify-content: flex-end;
+		align-items: flex-end;
 		text-align: right;
 	}
 
 	.received {
-		justify-content: flex-start;
+		align-items: flex-start;
 		text-align: left;
 	}
 
 	.message-text {
-		/* Estilo para o formado do elemento da mensagem */
 		background: #001837;
-		border-radius: 20px;
-		padding: 8px 14px;
-		white-space: pre-wrap !important;
-		overflow: visible !important;
+		border-radius: 12px;
+		padding: 4px 10px;
+		// white-space: pre-wrap !important;
+		// overflow: visible !important;
+		word-wrap: break-word;
 		min-width: 40px;
-		max-width: 70%;
+		max-width: 65%;
 		display: inline-block;
 	}
 
@@ -193,15 +230,28 @@
 	.message-time {
 		font-size: 0.75rem;
 		color: #8f8f8f;
-		margin-top: 2px;
-		padding-left: 48px;
+		margin-top: 1px;
+		font-style: italic;
+	}
+
+	.input-wrapper {
+		width: 100%;
+		display: flex;
+		justify-content: center;
 	}
 
 	.container-input {
-		position: absolute;
-		bottom: 18px;
-		min-width: 400px;
-		// padding: 16px;
-		// z-index: 10; /* Para garantir que fique acima de outros elementos */
+		margin-bottom: 8px;
+		width: 100%;
+		max-width: 40%; /* PCs: 40% da largura */
+	}
+
+	@media (max-width: 768px) {
+		.container-input {
+			max-width: 95%; /* Celulares: 100% da largura */
+		}
+		.message-text {
+			max-width: 95%;
+		}
 	}
 </style>
